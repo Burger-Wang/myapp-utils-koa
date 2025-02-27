7537f3a3ca82d81b04c414c9d113c327157b13b5
wangzeyu <wangzeyu@luckyxp.net>
2025-02-27 15:27:43 +0800

    一键提交

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import Koa from "koa";
+import dayjs from "dayjs"
 import Router from "@koa/router";
 import { bodyParser } from "@koa/bodyparser";
 import addModules from "./controller/index.js";
 import { routerResponse } from "./utils/routerResponse.js";
+
 
 const app = new Koa();
 app.use(bodyParser());
@@ -11,11 +13,12 @@ const router = new Router();
 await addModules(router, "controller");
 
 app.use(async (ctx, next) => {
-  ctx.body = 'Hello World';
+  ctx.body = 'Hello World, test Koa';
   await next();
 });
 
 console.log('我真的有更新啊')
+console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'))
 
 app.use(router.routes());
 app.listen(3000);
