2857029b33178312261710368c995b61e73a0c70
wangzeyu <wangzeyu@luckyxp.net>
2025-02-27 17:41:54 +0800

    一键提交

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const router = new Router();
 await addModules(router, "controller");
 
 app.use(async (ctx, next) => {
-  ctx.body = 'Hello World, test Koa, test update 2b326';
+  ctx.body = 'Hello World, test Koa, test update 2sb326';
   await next();
 });
 
